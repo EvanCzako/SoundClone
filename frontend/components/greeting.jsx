01443c37fe98b29b2390bc3d721f058bf35e7034
EvanCzako <evan.czako@gmail.com>
2021-09-08 13:23:51 -0400

    Made greeting a class component

--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Greeting = (props) => {
+class Greeting extends React.Component {
 
-    const sessionLinks = () => (
-        <nav className="login-signup">
-            <button onClick={() => props.openModal('login')}>Login</button>
-            <br />
-            <button onClick={() => props.openModal('signup')}>Signup</button>
-        </nav>
-    );
-
-    if (!!props.currentUser){
-        props.closeModal();
-        return (
-            <div>
-                <p>Hey, {props.currentUser.username}!</p>
-                <button onClick={props.logout}>Logout</button>
-            </div>
-        );
-    }
-    else {
-        return (
-            sessionLinks()
-        );
+    constructor(props){
+        super(props);
     }
 
-};
+    componentDidUpdate(){
+        this.props.closeModal();
+    }
+
+    render(){
+        const sessionLinks = () => (
+            <nav className="login-signup">
+                <button onClick={() => this.props.openModal('login')}>Login</button>
+                <br />
+                <button onClick={() => this.props.openModal('signup')}>Signup</button>
+            </nav>
+        );
+
+        if (!!this.props.currentUser) {
+            return (
+                <div>
+                    <p>Hey, {this.props.currentUser.username}!</p>
+                    <button onClick={this.props.logout}>Logout</button>
+                </div>
+            );
+        }
+        else {
+            return (
+                sessionLinks()
+            );
+        }
+    }
+
+
+}
 
 export default Greeting;
