382b131c87b044d7754bbb2b2784a31cf23dd818
EvanCzako <evan.czako@gmail.com>
2021-09-15 12:29:03 -0400

    Add audio bar functionality

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 
 import usersReducer from "./users_reducer";
 import tracksReducer from "./tracks_reducer";
+import currentTrackReducer from "./current_track_reducer";
 
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    tracks: tracksReducer
+    tracks: tracksReducer,
+    currentTrack: currentTrackReducer
 })
 
 export default entitiesReducer;
